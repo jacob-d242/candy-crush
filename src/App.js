dfa1da969e07913e110d2d872261b1373dc2e7df
jacob-d242 <jacobmoracha@gmail.com>
2022-01-14 20:05:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,76 @@ const App = () => {
 
   const [ currentColorArrangement, setcurrentColorArrangement] = useState([])
 
+    const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i+width*3]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+
+    }
+  } 
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i+2, i+3 ]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5,6, 7, 13,14, 15, 21,22, 23, 29,30, 31, 37,38, 39, 46,47, 47, 53,54, 55,62, 63, 64]
+      if (notValid.includes(i)) continue
+      if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+
+    }
+  } 
+
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+
+    }
+  } 
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i+2 ]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 47, 47, 54, 55, 63, 64]
+      if (notValid.includes(i)) continue
+      if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+
+    }
+  } 
+
+  const moveIntoSquareBelow = () => {
+    for (let i = 0; i < 64 - width; i++) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+
+      const isFirstRow = firstRow.includes(i)
+
+      if (isFirstRow && currentColorArrangement[i] === '' ) {
+        let randomNumber = Math.floor(Math.random() * candyColors.length)
+        currentColorArrangement[i] =candyColors[randomNumber]
+      }
+
+      if ((currentColorArrangement[i+ width] ) === '')
+        currentColorArrangement[i + width] = currentColorArrangement[i]
+        currentColorArrangement[i] = ''
+
+    }
+  }
+
+
   const createBoard = () => {
     const randomColorArrangement=[]
     for (let i = 0; i < width * width; i++){
@@ -26,7 +96,22 @@ const App = () => {
   useEffect(() => {
     createBoard()
   }, [])
+  useEffect(() => {
+    
+    const timer = setInterval(() => {
+      checkForColumnOfFour()
+      checkForRowOfFour()
+      checkForColumnOfThree()
+      checkForRowOfThree()
+      moveIntoSquareBelow()
+      setcurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+
+
+  }, [checkForColumnOfFour,checkForRowOfFour,checkForColumnOfThree,checkForRowOfThree,moveIntoSquareBelow,currentColorArrangement])
   
+
   console.log(currentColorArrangement)
 
   return (
